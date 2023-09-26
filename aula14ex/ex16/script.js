41178e5cac2dcd4b9a7d5eca0241cd4a4cd67a61
Guilherme Evangelista <109614158+guilhermeev6@users.noreply.github.com>
2023-09-26 10:36:57 -0300

    Update script.js

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,11 +1,14 @@
-function Calcular(){
-    let inicio = parseInt(document.getElementById('inicio').value)
-    let fim = parseInt(document.getElementById('fim').value)
-    let passo = parseInt(document.getElementById('passo'))
-    let result = document.getElementById('resultado')
+function Calcular() {
+  let inicio = parseInt(document.getElementById("inicio").value);
+  let fim = document.getElementById("fim");
+  let passo = document.getElementById("passo");
+  let result = document.getElementById("resultado");
 
+  let f = Number(fim.value);
+  let p = Number(passo.value);
 
-    for(c = inicio; c <= fim; c = c + passo){
-        result.innerHTML += c
-    }
+  result.innerHTML = "";
+  for (let c = inicio; c <= f; c = c + p) {
+    result.innerHTML += ` ${c} `;
+  }
 }
