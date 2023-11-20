00f9a39add26c84577d0acc69842d230a8b8a5d6
guilhermeev6 <guilhermeevts@gmail.com>
2023-11-20 14:22:42 -0300

    att files

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,14 +1,10 @@
-function Calcular() {
-  let inicio = parseInt(document.getElementById("inicio").value);
-  let fim = document.getElementById("fim");
-  let passo = document.getElementById("passo");
-  let result = document.getElementById("resultado");
+function Calcular(){
+  let inicio = parseInt(document.getElementById('inicio').value)
+  let fim = parseInt(document.getElementById('fim').value)
+  let passo = parseInt(document.getElementById('passo').value)
+  let result = document.getElementById('resultado')
 
-  let f = Number(fim.value);
-  let p = Number(passo.value);
-
-  result.innerHTML = "";
-  for (let c = inicio; c <= f; c = c + p) {
-    result.innerHTML += ` ${c} `;
+  for(c = inicio; c <= fim; c = c + passo){
+      result.innerHTML += c
   }
 }
