854ac98e76d20358788cc7814309f830405afa16
guilhermeev6 <guilhermeevts@gmail.com>
2024-01-03 16:04:36 -0300

    finished

---
+++ styleInJs/script.js
@@ -0,0 +1,8 @@
+let button =  document.querySelector('button')
+
+button.addEventListener("click", () => {button.style.backgroundColor = "red"})
+
+
+let div = document.getElementById('guiguiba')
+
+div.addEventListener("click", () => {div.classList.toggle("estilo-predefinido")})
